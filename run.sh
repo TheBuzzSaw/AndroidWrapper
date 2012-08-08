3ba684ee2423d430216b9cecbba44ca8d1ca8d74
Matt <matt@Ronin.(none)>
2012-08-08 13:09:02 -0600

    Changing the project to be Zero2D, There is a problem here

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
-ant installd && adb shell am start -a android.intent.action.MAIN -n com.example.native_activity/android.app.NativeActivity
+ant installd && adb shell am start -a android.intent.action.MAIN -n com.example.native_activity/android.app.Zero2D
 
